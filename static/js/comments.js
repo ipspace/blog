6631c52b06a08b3b97cc23add3ff273397130477
Ivan Pepelnjak <ip@ipspace.net>
2020-04-08 17:15:36 +0200

    Remove duplicate comments from RECENT COMMENTS list

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,3 +1,19 @@
+function remove_duplicate_comments() {
+  var recent = $("#recent_comments");
+  var static = $("#comments div.comments-content").first()
+
+  recent.find("li.comment").each(function() {
+    var me = $(this);
+    var sme = static.find('li[id='+me.attr('id')+']');
+    if (sme.length) {
+      me.remove();
+    }
+  });
+  if (recent.find("li.comment").length == 0) {
+    recent.remove();
+  }
+}
+
 $(function() {
   var server = 'https://my.ipspace.net/';
   if (location.host.indexOf('localhost') >= 0) {
@@ -11,6 +27,6 @@ $(function() {
   if (comments.length) {
     var recent = $("<div id='recent_comments' style='clear: both;'></div>");
     recent.appendTo($("#comments"));
-    recent.load(server+"bin/comment/list");
+    recent.load(server+"bin/comment/list",remove_duplicate_comments);
   }
 });
